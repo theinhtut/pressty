8f60f5835ef62e76360d0ab600e930bc789694db
Thein Htut <theinhtut.dec@gmail.com>
2020-07-19 21:59:51 +0800

    Installed dotenv setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-const express = require('express')
+import express from "express"
 
 const app = express()
 
 const port = process.env.PORT || 3000
 
-app.get('/', (req: any, res: any) => {
+app.get('/', (req, res) => {
   res.send('Releasing changes to test: CodePipeline --> Elastic Beanstalk')
 })
 
---
+++ src/utils/dotenv.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv"
+
+let path
+
+if (process.env.NODE_ENV === 'production') {
+  path = `${__dirname}/../../.env.production`
+} else {
+  path = `${__dirname}/../../.env.development`
+}
+
+dotenv.config({path})
+
+export const DB_HOST = process.env.DB_HOST
+export const DB_NAME = process.env.DB_NAME
+export const DB_PORT = process.env.DB_PORT
+export const DB_USERNAME = process.env.DB_USERNAME
+export const DB_PASS = process.env.DB_PASS
